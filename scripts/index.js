1166f49af2aabba1e798aa2a049dc36c0d6a467c
Eugenia Savchenko <eugen1ya.smith@yandex.ru>
2023-02-03 19:42:17 +0300

    Исправление

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,22 @@
-let profile = document.querySelector(".profile");
-let editButton = profile.querySelector(".profile__edit-button");
-let profileTitle = profile.querySelector(".profile__title");
-let profileText = profile.querySelector(".profile__text");
-let popup = document.querySelector(".popup");
-let closeButton = popup.querySelector(".popup__close-button");
-let formElement = document.querySelector(".form");
-let nameInput = form.querySelector(".form__input_name");
-let jobInput = form.querySelector(".form__input_about");
-let submitButton = form.querySelector(".form__edit-button");
+let profile = document.querySelector('.profile');
+let editButton = profile.querySelector('.profile__edit-button');
+let profileTitle = profile.querySelector('.profile__title');
+let profileText = profile.querySelector('.profile__text');
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close-button');
+let formElement = document.querySelector('.form');
+let nameInput = form.querySelector('.form__input_type_name');
+let jobInput = form.querySelector('.form__input_type_about');
+let submitButton = form.querySelector('.form__edit-button');
 
 
 function handleEditButtonClick () {
-    popup.classList.add("popup_opened");
+    popup.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileText.textContent;
 }
 function handleCloseButtonClick () {
-    popup.classList.remove("popup_opened");
+    popup.classList.remove('popup_opened');
 }
 function handleFormSubmit (evt) {
     evt.preventDefault();
@@ -22,6 +24,6 @@ function handleFormSubmit (evt) {
     profileText.textContent = jobInput.value;
     handleCloseButtonClick();
 }
-editButton.addEventListener("click", handleEditButtonClick);
-closeButton.addEventListener("click", handleCloseButtonClick);
+editButton.addEventListener('click', handleEditButtonClick);
+closeButton.addEventListener('click', handleCloseButtonClick);
 formElement.addEventListener('submit', handleFormSubmit);

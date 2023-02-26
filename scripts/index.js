a3ded972d93a46b5455b5bf9580099e0c7f7c1d7
Eugenia Savchenko <eugen1ya.smith@yandex.ru>
2023-02-26 21:53:02 +0300

    Исправления 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const profile = document.querySelector('.profile');
 const profileEditButton = profile.querySelector('.profile__edit-button');
-let profileTitle = profile.querySelector('.profile__title');
+const profileTitle = profile.querySelector('.profile__title');
 const profileAddButton = document.querySelector('.profile__add-button');
 const closeAddButton = document.querySelector('.popup__close-button_type_add');
 const popupAdd = document.querySelector('.popup_type_add');
@@ -8,14 +8,13 @@ const formAdd = document.forms['form-add'];
 const inputTitle = formAdd.querySelector('.form__input_type_title');
 const inputLink = formAdd.querySelector('.form__input_type_link');
 const saveAddButton = formAdd.querySelector('.form__edit-button_type_save');
-const popup = document.querySelector('.popup');
-let profileText = profile.querySelector('.profile__text');
+const profileText = profile.querySelector('.profile__text');
 const profilePopup = document.querySelector('.popup_type_profile');
 const profileCloseButton = profilePopup.querySelector('.popup__close-button');
 const profileForm = document.forms['form-profile'];
-let nameInput = profileForm.querySelector('.form__input_type_name');
-let jobInput = profileForm.querySelector('.form__input_type_about');
-let initialCards = [
+const nameInput = profileForm.querySelector('.form__input_type_name');
+const jobInput = profileForm.querySelector('.form__input_type_about');
+const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -44,8 +43,8 @@ let initialCards = [
 ];
 const popupImage = document.querySelector('.popup_type_images');
 const closeButtonImage = popupImage.querySelector('.popup__close-button-image');
-let image = popupImage.querySelector('.popup__image');
-let imageName = popupImage.querySelector('.popup__image-name');
+const image = popupImage.querySelector('.popup__image');
+const imageName = popupImage.querySelector('.popup__image-name');
 const elements = document.querySelector('.elements');
 const template = document.getElementById('template__elements');
 
@@ -68,8 +67,8 @@ profileForm.addEventListener('submit', handleProfileFormSubmit);
 
 const getCard = (item) => {
   const cardElement = template.content.cloneNode(true);
-  let cardTitle = cardElement.querySelector('.elements__title');
-  let cardImage = cardElement.querySelector('.elements__image');
+  const cardTitle = cardElement.querySelector('.elements__title');
+  const cardImage = cardElement.querySelector('.elements__image');
   cardTitle.textContent = item.name;
   cardImage.src = item.link;
   cardImage.alt = item.name;
